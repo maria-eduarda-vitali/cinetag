852324960f0865a3c830a41b13e2cd99c5b29efb
Maria Eduarda Vitali <mariaeduarda.vitali.ifpr@gmail.com>
2023-02-24 09:38:41 -0300

    first commit

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css';
+
+import React from 'react'
+
+export default function Rodape() {
+  return (
+    <footer className={styles.rodape}>
+        <h2>Desenvolvido por Vitali</h2>
+    </footer>
+  )
+}
